c8cb2c863ea7f68ddade2b41d589b7f4e82305aa
Kshoes <kx360@nyu.edu>
2021-07-01 23:48:25 -0400

    first commit

---
+++ src/components/MusicPlayer/index.js
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import React, {useReducer, createContext} from 'react'
+import { css, jsx } from '@emotion/core'
+import Topbar from './Topbar'
+import Sidebar from './Sidebar'
+import Content from './Content'
+import Playbar from './Playbar'
+import media from '../../media.json'
+
+export const StoreContext = createContext(null)
+
+const DEFAULT_PLAYLIST = "home";
+
+const initialState = {
+  currentPlaylist: DEFAULT_PLAYLIST,
+  media,
+  playlists: {
+    home: new Set(media.ids),
+    favorites: new Set(),
+  },
+};
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'ADD_PLAYLIST':
+            return {
+                ...state, 
+                playlists: {...state.playlists, [action.playlist]: new Set() }
+            }
+        case 'SET_PLAYLIST':
+            return {
+                ...state,
+                currentPlaylist: action.playlist
+            }
+    }
+    return state
+}
+
+const MusicPlayer = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <StoreContext.Provider value={{state, dispatch}} >
+        <div className="MusicPlayer" css={CSS}>
+            <Topbar />
+            <Sidebar />
+            <Content />
+            <Playbar></Playbar>
+        </div>
+    </StoreContext.Provider>
+  )
+}
+
+const CSS = css`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    position: relative;
+    color: white;
+`
+
+export default MusicPlayer
